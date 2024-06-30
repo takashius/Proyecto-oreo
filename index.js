684d6b28239b53d6188c0733114e3eedd9c56a21
FJcarreazo <159186912+FJcarreazo@users.noreply.github.com>
2024-06-29 20:28:57 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const http = require('http');
-
-
-const server = http.createServer(app);
-
-server.listen(3003, () => {
-    console.log('El servidor esta corriendo');
-})
