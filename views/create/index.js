31aeaec8ca4930b2309356ef2b779ec472941b15
FJcarreazo <FJ21carreazo@gmail.com>
2024-07-14 11:19:05 -0400

    Apartado para crear el producto

---
+++ views/create/index.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('#formulario')
+const nameProduct = document.querySelector('#name-product');
+const descripcion = document.querySelector('#descripcion');
+const precio = document.querySelector('#precio');
+const foto = document.querySelector('#foto');
+const submit = document.querySelector('#submit-btn');
+
+
+
