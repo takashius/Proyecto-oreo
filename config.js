ecd8f39cf8bc64475be8c7e02a0cb7966cf75319
FJcarreazo <159186912+FJcarreazo@users.noreply.github.com>
2024-07-15 10:31:34 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
- ? 'placeholder'
- : 'http://localhost:3003';
+    ? 'https://proyecto-oreo.onrender.com'
+    : 'http://localhost:3003';
 
- const MONGO_URI = process.env.NODE_ENV === 'production'
- ? process.env.MONGO_URI_PROD
- : process.env.MONGO_URI_TEST
-  
+const MONGO_URI = process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_URI_PROD
+    : process.env.MONGO_URI_TEST
 
 
- module.exports = { PAGE_URL, MONGO_URI };
+
+module.exports = { PAGE_URL, MONGO_URI };
